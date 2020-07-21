1e7a1424607e19a3da06220129b2b2e9f024b81b
Erik Schaal <schaa077@umn.edu>
2020-07-21 09:42:12 -0500

    configured connection file for heroku

--- db/connection.js
+++ db/connection.js
@@ -3,31 +3,35 @@ const util = require('util');
 //const inquirer = require("inquirer");
 //const cTable = require('console.table');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "Sch@@123",
-    database: "noteTaker_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    var connection = mysql.createConnection({
+        host: "localhost",
+
+        // Your port; if not 3306
+        port: 3306,
+
+        // Your username
+        user: "root",
+
+        // Your password
+        password: "Sch@@123",
+        database: "noteTaker_db"
+    });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
     console.log("connected as id " + connection.threadId);
-  });
-  
-  connection.query = util.promisify(connection.query);
-  
-  // Export connection for our ORM to use.
-  module.exports = connection;
-  
+});
+
+connection.query = util.promisify(connection.query);
+
+// Export connection for our ORM to use.
+module.exports = connection;
